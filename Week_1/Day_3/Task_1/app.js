55cdf6dca3636bc2052cb539c3a7a602cc9afc54
Aman Upadhyay <amanupadhyay0208@gmail.com>
2022-07-06 17:45:42 +0530

    Add files via upload

--- Week_1/Day_3/Task_1/app.js
+++ Week_1/Day_3/Task_1/app.js
@@ -1,3 +1,17 @@
-function CheckNumber() {
+        var y = Math.floor(Math.random() * 10 + 1);
+        var guess = 1;
 
-}
+        function CheckNumber(){
+            var x = document.getElementById("UserInput").value;
+            if(x == y){
+                document.getElementById('Result').innerHTML = 'Congratulations! You Guessed It Right in ' + guess + ' Guesses';
+            }
+            else if(x > y){
+                guess++;
+                document.getElementById('Result').innerHTML = 'Sorry Number is Smaller';
+            }
+            else{
+                guess++;
+                document.getElementById('Result').innerHTML = 'Sorry Number is Greater';
+            }
+        }
